f6f6cd66a11a79aecb724f6c0514e7c6edfcf686
David <dcarretero86@hotmail.com>
2020-12-24 18:26:12 +0100

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -11,7 +11,7 @@ return new \Phalcon\Config([
         'adapter'     => 'Mysql',
         'host'        => 'localhost',
         'username'    => 'root',
-        'password'    => 'david99',
+        'password'    => '',
         'dbname'      => 'ovnisreales',
         'charset'     => 'utf8',
     ],
